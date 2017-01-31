8f8f3623d178a4c4ddf8ac11c8a94c39e4b29e30
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-01-31 08:01:09 -0800

    Make shutoff timeout configurable

--- openag_binary_actuator.cpp
+++ openag_binary_actuator.cpp
@@ -1,8 +1,9 @@
 #include "openag_binary_actuator.h"
 
-BinaryActuator::BinaryActuator(int pin, bool is_active_low) {
+BinaryActuator::BinaryActuator(int pin, bool is_active_low, int shutoff_ms) {
   _pin = pin;
   _is_active_low = is_active_low;
+  _shutoff_ms = shutoff_ms;
   _last_cmd = 0;
   status_level = OK;
   status_code = CODE_OK;
@@ -21,7 +22,7 @@ void BinaryActuator::begin() {
 
 void BinaryActuator::update() {
   uint32_t curr_time = millis();
-  if ((curr_time - _last_cmd) > _max_update_interval) {
+  if ((curr_time - _last_cmd) > _shutoff_ms) {
     if (_is_active_low) {
       digitalWrite(_pin, HIGH);
     }
--- openag_binary_actuator.h
+++ openag_binary_actuator.h
@@ -8,7 +8,7 @@
 class BinaryActuator : public Module {
   public:
     // Constructor
-    BinaryActuator(int pin, bool is_active_low);
+    BinaryActuator(int pin, bool is_active_low, int shutoff_ms);
 
     // Public functions
     void begin();
@@ -19,8 +19,8 @@ class BinaryActuator : public Module {
     // Private variables
     int _pin;
     bool _is_active_low;
+    int _shutoff_ms = 10000;
     uint32_t _last_cmd;
-    const static int _max_update_interval = 10000;
 };
 
 #endif
